e2e670ee8d3168a4519bf317ac7991fc23ad5c23
Ricardo <ricardomorarey@gmail.com>
2019-04-06 20:55:02 +0200

    interface chica

---
+++ app/src/main/java/com/ricardo/interfaces/ChicaLayout.kt
@@ -0,0 +1,12 @@
+package com.ricardo.interfaces
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ChicaLayout : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_chica_layout)
+    }
+}
--- app/src/main/java/com/ricardo/interfaces/LoginActivity.kt
+++ app/src/main/java/com/ricardo/interfaces/LoginActivity.kt
@@ -1,12 +1,19 @@
 package com.ricardo.interfaces
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        button.setOnClickListener{view ->
+            val intento1 = Intent(this, ChicaLayout::class.java)
+            startActivity(intento1)
+        }
     }
 }
