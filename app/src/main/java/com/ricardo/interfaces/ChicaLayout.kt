5285c179acd04179f0a2ddea6395b3076beced81
Ricardo <ricardomorarey@gmail.com>
2019-04-08 17:00:00 +0200

    componente

--- app/src/main/java/com/ricardo/interfaces/ChicaLayout.kt
+++ app/src/main/java/com/ricardo/interfaces/ChicaLayout.kt
@@ -1,12 +1,30 @@
 package com.ricardo.interfaces
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import androidx.drawerlayout.widget.DrawerLayout
+import kotlinx.android.synthetic.main.activity_chica_layout.*
 
 class ChicaLayout : AppCompatActivity() {
+
+    private val mDrawerLayout: DrawerLayout? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chica_layout)
+
+        floatingActionButton.setOnClickListener{ view ->
+            //val intent = Intent (this, MisComponentes::class.java)
+            //startActivity(intent)
+        }
+
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.nav_options, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
 }
---
+++ app/src/main/java/com/ricardo/interfaces/MiComponente.kt
@@ -0,0 +1,10 @@
+package com.ricardo.interfaces
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+
+class MiComponente(context: Context, attr: AttributeSet ): View(context, attr) {
+
+
+}
