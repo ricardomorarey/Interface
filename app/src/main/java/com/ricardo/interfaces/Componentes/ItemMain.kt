86f4bad4bacae5c46477c095b2ae2e079bcce6ff
Ricardo <ricardomorarey@gmail.com>
2019-04-09 18:58:07 +0200

    no consigo cargar la imageview

--- app/src/main/java/com/ricardo/interfaces/Componentes/ItemMain.kt
+++ app/src/main/java/com/ricardo/interfaces/Componentes/ItemMain.kt
@@ -6,23 +6,27 @@ import android.view.View
 import android.widget.EditText
 import android.widget.ImageView
 import android.widget.LinearLayout
+import com.google.android.material.textfield.TextInputLayout
 import com.ricardo.interfaces.R
+import kotlinx.android.synthetic.main.item_input_text.view.*
 
 class ItemMain(context: Context?, attrs: AttributeSet?): LinearLayout(context, attrs) {
 
-    private val icon: ImageView
-    private val editText: EditText
+    private val iconc: ImageView
+    private val editText: TextInputLayout
 
     init {
         View.inflate(this.context, R.layout.item_input_text,this)
 
-        icon = findViewById(R.id.imageView7)
+        iconc = findViewById(R.id.iv_item)
         editText = findViewById(R.id.editText6)
 
         attrs?.let {
             val typeArray = context!!.obtainStyledAttributes(it,
                 R.styleable.ItemMain, 0, 0)
             editText.hint = typeArray.getString(R.styleable.ItemMain_im_et)
+            val i = typeArray.getDrawable(R.styleable.ItemMain_im_image)
+            iconc.setImageDrawable(i)
         }
 
     }
